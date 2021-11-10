7e00456f67c4d605291d8251be65c542fb13978a
yaolong.ye <470080502@qq.com>
2021-11-10 21:04:59 +0800

    暂存

--- start/lecture3_2/app.js
+++ start/lecture3_2/app.js
@@ -1,20 +1,20 @@
 import * as THREE from '../../libs/three126/three.module.js';
 import { OrbitControls } from '../../libs/three126/OrbitControls.js';
 
-class App{
-	constructor(){
-		const container = document.createElement( 'div' );
-		document.body.appendChild( container );
-    
-        window.addEventListener('resize', this.resize.bind(this) );
-	}	
-    
-    resize(){
-        
+class App {
+    constructor() {
+        const container = document.createElement('div');
+        document.body.appendChild(container);
+
+        window.addEventListener('resize', this.resize.bind(this));
     }
-    
-	render( ) {   
-        
+
+    resize() {
+
+    }
+
+    render() {
+
     }
 }
 
--- start/lecture3_3/app.js
+++ start/lecture3_3/app.js
@@ -1,20 +1,25 @@
 import * as THREE from '../../libs/three126/three.module.js';
 import { OrbitControls } from '../../libs/three126/OrbitControls.js';
 
-class App{
-	constructor(){
-		const container = document.createElement( 'div' );
-		document.body.appendChild( container );
+class App {
+    constructor() {
+        const container = document.createElement('div');
+        document.body.appendChild(container);
+        this.camera = new THREE.PerspectiveCamera(60, window.innerWidth, window.innerHeight, .1, 100);
 
-        window.addEventListener('resize', this.resize.bind(this) );
-	}	
-    
-    resize(){
+        this.camera.position.set(0, 0, 0, 4);
+
+        this.scene = new THREE.Scene();
+        // this.scene.background = new THREE.
+        window.addEventListener('resize', this.resize.bind(this));
+    }
+
+    resize() {
 
     }
-    
-	render( ) {   
-		
+
+    render() {
+
     }
 }
 
